31bb7805f80c549e2485fa368d7a45adb0cc3ec4
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-04-12 18:10:24 +0200

    Separated data from algos

--- recommendations.py
+++ recommendations.py
@@ -1,63 +1,7 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 import numpy as np
 from numpy import linalg as LA
-
-critics = {
-    "Lisa Rose": {
-        "Lady in the Water": 2.5,
-        "Snakes on a Plane": 3.5,
-        "Just My Luck": 3.0,
-        "Superman Returns": 3.5,
-        "You, Me and Dupree": 2.5,
-        "The Night Listener": 3.0,
-    },
-    "Gene Seymour": {
-        "Lady in the Water": 3.0,
-        "Snakes on a Plane": 3.5,
-        "Just My Luck": 1.5,
-        "Superman Returns": 5.0,
-        "The Night Listener": 3.0,
-        "You, Me and Dupree": 3.5,
-    },
-    "Michael Phillips": {
-        "Lady in the Water": 2.5,
-        "Snakes on a Plane": 3.0,
-        "Superman Returns": 3.5,
-        "The Night Listener": 4.0,
-    },
-    "Claudia Puig": {
-        "Snakes on a Plane": 3.5,
-        "Just My Luck": 3.0,
-        "The Night Listener": 4.5,
-        "Superman Returns": 4.0,
-        "You, Me and Dupree": 2.5,
-    },
-    "Mick LaSalle": {
-        "Lady in the Water": 3.0,
-        "Snakes on a Plane": 4.0,
-        "Just My Luck": 2.0,
-        "Superman Returns": 3.0,
-        "The Night Listener": 3.0,
-        "You, Me and Dupree": 2.0,
-    },
-    "Jack Matthews": {
-        "Lady in the Water": 3.0,
-        "Snakes on a Plane": 4.0,
-        "The Night Listener": 3.0,
-        "Superman Returns": 5.0,
-        "You, Me and Dupree": 3.5,
-    },
-    "Toby": {
-        "Snakes on a Plane": 4.5,
-        "You, Me and Dupree": 1.0,
-        "Superman Returns": 4.0,
-    },
-}
-
-
-names = set(critics.keys())
-movies = set().union(*(critics[n].keys() for n in names))
 
 
 def get_points(user1: str, user2: str, prefs: Dict[str, Dict]) -> Tuple[nd.array]:
@@ -71,18 +15,15 @@ def get_points(user1: str, user2: str, prefs: Dict[str, Dict]) -> Tuple[nd.array
     return p1, p2
 
 
-def similarity_distance(
-    user1: str, user2: str, prefs: Dict[str, Dict] = critics
-) -> float:
+def similarity_distance(user1: str, user2: str, prefs: Dict[str, Dict]) -> float:
 
     p1, p2 = get_points(user1, user2, prefs)
 
     distance = np.sqrt(((p2 - p1) ** 2).sum())
     return 1.0 / (1.0 + distance)
 
-def similarity_pearson(
-    user1: str, user2: str, prefs: Dict[str, Dict] = critics
-) -> float:
+
+def similarity_pearson(user1: str, user2: str, prefs: Dict[str, Dict]) -> float:
 
     p1, p2 = get_points(user1, user2, prefs)
     n = len(p1)
