fae386eb1aac5b2b0659eb7d1975c682c8a82bf1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-04-12 18:06:03 +0200

    Added pearson metric

--- recommendations.py
+++ recommendations.py
@@ -60,13 +60,38 @@ names = set(critics.keys())
 movies = set().union(*(critics[n].keys() for n in names))
 
 
-def sim_distance(person1: str, person2: str, prefs: Dict[str, Dict] = critics) -> float:
+def get_points(user1: str, user2: str, prefs: Dict[str, Dict]) -> Tuple[nd.array]:
+    common = set(prefs[user1]).intersection(prefs[user2])
+    if not common:
+        return 0
 
-    movies = set(prefs[person1]).intersection(prefs[person2])
-    
-    p1 = np.array([prefs[person1][m] for m in movies])
-    p2 = np.array([prefs[person2][m] for m in movies])
+    # points
+    p1 = np.array([prefs[user1][item] for item in common])
+    p2 = np.array([prefs[user2][item] for item in common])
+    return p1, p2
+
+
+def similarity_distance(
+    user1: str, user2: str, prefs: Dict[str, Dict] = critics
+) -> float:
+
+    p1, p2 = get_points(user1, user2, prefs)
 
     distance = np.sqrt(((p2 - p1) ** 2).sum())
     return 1.0 / (1.0 + distance)
 
+def similarity_pearson(
+    user1: str, user2: str, prefs: Dict[str, Dict] = critics
+) -> float:
+
+    p1, p2 = get_points(user1, user2, prefs)
+    n = len(p1)
+
+    def _var(x: nd.array, y: nd.array) -> float:
+        return (x * y).sum() - (x.sum() * y.sum() / n)
+
+    # how much variables change together
+    cross = _var(p1, p2)
+    # product of individual variations
+    individual = np.sqrt(_var(p1, p1) * _var(p2, p2))
+    return cross / individual
