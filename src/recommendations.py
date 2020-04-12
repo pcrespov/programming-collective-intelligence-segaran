bb1d1a3665d3131286a825c25f6fd622a6fe34fc
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-04-12 23:49:44 +0200

    Recommending items

--- src/recommendations.py
+++ src/recommendations.py
@@ -1,14 +1,19 @@
+from collections import defaultdict
 from operator import itemgetter
-from typing import Dict, Tuple, List
+from typing import Dict, List, Tuple
 
 import numpy as np
-from numpy import linalg as LA
+
+# from numpy import linalg as LA
 
 
 def get_points(user1: str, user2: str, prefs: Dict[str, Dict]) -> Tuple[np.array]:
+    if user1 not in prefs or user2 not in prefs:
+        raise ValueError
+
     common = sorted(set(prefs[user1]).intersection(prefs[user2]))
     if not common:
-        return 0
+        raise ValueError
 
     # points
     p1 = np.array([prefs[user1][item] for item in common])
@@ -18,7 +23,10 @@ def get_points(user1: str, user2: str, prefs: Dict[str, Dict]) -> Tuple[np.array
 
 def similarity_distance(user1: str, user2: str, *, prefs: Dict[str, Dict]) -> float:
 
-    p1, p2 = get_points(user1, user2, prefs)
+    try:
+        p1, p2 = get_points(user1, user2, prefs)
+    except ValueError:
+        return 0
 
     distance = np.sqrt(((p2 - p1) ** 2).sum())
     return 1.0 / (1.0 + distance)
@@ -26,11 +34,17 @@ def similarity_distance(user1: str, user2: str, *, prefs: Dict[str, Dict]) -> fl
 
 def similarity_pearson(user1: str, user2: str, *, prefs: Dict[str, Dict]) -> float:
 
-    p1, p2 = get_points(user1, user2, prefs)
-    n = len(p1)
+    try:
+        p1, p2 = get_points(user1, user2, prefs)
+    except ValueError:
+        return 0
+
+    dim = len(p1)
+    if dim <= 1:  # TODO: checkcase dim == 1! => 0/0
+        return 0
 
     def _var(x: np.array, y: np.array) -> float:
-        return (x * y).sum() - (x.sum() * y.sum() / n)
+        return (x * y).sum() - (x.sum() * y.sum() / dim)
 
     # how much variables change together
     cross = _var(p1, p2)
@@ -40,7 +54,7 @@ def similarity_pearson(user1: str, user2: str, *, prefs: Dict[str, Dict]) -> flo
     return cross / individual
 
 
-SIMILARITY_INDEX = 0
+SCORE_INDEX = 0
 
 
 def eval_top_matches(
@@ -53,5 +67,36 @@ def eval_top_matches(
         if other != user
     ]
 
-    scores.sort(key=itemgetter(SIMILARITY_INDEX), reverse=True)
+    scores.sort(key=itemgetter(SCORE_INDEX), reverse=True)
+    return scores[:count]
+
+
+def get_recommendations(
+    user: str, *, count: int = 5, similarity=similarity_pearson, prefs: Dict[str, Dict]
+) -> List[Tuple[float, str]]:
+    """  Returns a list of recommendations of items the user has not scored """
+
+    def _nill():
+        return 0
+
+    rating = defaultdict(_nill)
+    totals = defaultdict(_nill)
+
+    user_rating = prefs.get(user, {})
+
+    for other in prefs:
+        if other != user:
+            sim = similarity(user, other, prefs=prefs)
+
+            if sim > 0:
+                for name, score in prefs[other].items():
+                    if name not in user_rating:
+                        rating[name] += sim * score
+                        totals[name] += sim
+
+    # normalize and encapsulate
+    scores = [(rating[name] / totals[name], name) for name in rating]
+
+    scores.sort(key=itemgetter(SCORE_INDEX), reverse=True)
+
     return scores[:count]
