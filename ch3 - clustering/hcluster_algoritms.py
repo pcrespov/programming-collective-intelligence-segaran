6146638d11275560c37408bbaa1bbe4252ecd2fa
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-08-01 16:09:26 +0200

    format

--- ch3 - clustering/hcluster_algoritms.py
+++ ch3 - clustering/hcluster_algoritms.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-# SEE https://www.python.org/dev/peps/pep-0563/
-
 import logging
-
+import random
 from typing import Any, List, Optional
 
 import attr
 import numpy as np
 
 from metrics import pearson_distance
+
+# SEE https://www.python.org/dev/peps/pep-0563/
+
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -72,3 +73,32 @@ def hcluster(table: List[List[Any]], distance_fun=pearson_distance) -> BiNode:
         items.append(cluster)
 
     return items[0] if items else None
+
+
+def scaledown(data, distance_fun=pearson_distance):
+    """
+        multidimensional scaling: representing n-dimensional space
+        in a 2d plane 
+    """
+    items = [np.array(row[:]) for row in data]
+    N = len(items)
+
+    distance = [[0.0 for i in range(N)] for j in range(N)]
+
+    for i, src in enumerate(items):
+        for j, dst in enumerate(items[i + 1 :], start=i + 1):
+            distance[i][j] = distance[j][i] = distance_fun(src, dst)
+
+    # set n random 2D points
+    points = [np.array([random.random(), random.random()]) for i in range(N)]
+
+    count = 0
+    while count < 100:
+        for i, p0 in enumerate(points):
+            grad = np.zeros((1, 2))
+            for j, p1 in enumerate(points[i + 1 :], start=i + 1):
+                errorij = distance_fun(p0, p1) - distance[i][j]
+                grad += (p0 - p1) / distance[i][j] * errorij
+            points[i] += grad
+
+    count += 1
